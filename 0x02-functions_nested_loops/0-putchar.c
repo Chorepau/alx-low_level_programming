793de762be9985552174c81377f7c47eb14635e6
Pauline Chore <chavaregichebet@gmail.com>
2022-09-12 17:53:25 -0700

    0-putchar.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,18 @@
+#include "0-putchar.c"
+
+/**
+* main - function to print
+*
+* Return 0
+*/
+int main(void)
+{
+char _putchar[9] - {'H", 'o', '1', 'b', 'e', 'r', 't', 'o', 'n', 'n'};
+unsigned int c;
+
+for(c = 0; c < sizeof(_putchar); c++)
+{
+_putchar(_putchar[c]);
+}
+_putchar('\n');
+Return (0);
